bdffb638a737e9ec20e5eda977accc4a4ac2cf38
Kevin Isom <kevin.isom@gmail.com>
2015-02-01 14:54:18 +1300

    rebuild

--- bundle.js
+++ bundle.js
@@ -20,12 +20,19 @@ game.globals = {
 
 game.state.add('level-1', require('./states/play.js'));
 game.state.add('level-2', require('./states/play.js'));
+game.state.add('level-3', require('./states/play.js'));
+game.state.add('level-4', require('./states/play.js'));
+game.state.add('level-5', require('./states/play.js'));
+game.state.add('level-6', require('./states/play.js'));
+game.state.add('end', require('./states/end.js'));
+game.state.add('dead', require('./states/dead.js'));
 game.state.add('load', require('./states/load.js'));
 game.state.add('menu', require('./states/menu.js'));
 game.state.add('boot', require('./states/boot.js'));
+game.state.add('intro', require('./states/intro.js'));
 game.state.start('boot');
 
-},{"./states/boot.js":4,"./states/load.js":5,"./states/menu.js":6,"./states/play.js":7}],2:[function(require,module,exports){
+},{"./states/boot.js":4,"./states/dead.js":5,"./states/end.js":6,"./states/intro.js":7,"./states/load.js":8,"./states/menu.js":9,"./states/play.js":10}],2:[function(require,module,exports){
 /*!
  * EventEmitter2
  * https://github.com/hij1nx/EventEmitter2
@@ -845,15 +852,84 @@ module.exports = {
         game.load.image('loading', 'assets/loading.png');
         game.load.image('load_progress_bar', 'assets/progress_bar_bg.png');
         game.load.image('load_progress_bar_dark', 'assets/progress_bar_fg.png');
+        game.load.bitmapFont('bits-1', 'assets/fonts/bits1.png', 'assets/fonts/bits1.fnt');
     },
 
     create: function () {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.state.start('load');
+        game.state.start('intro');
     }
 };
 
 },{}],5:[function(require,module,exports){
+module.exports = {
+    init: function () {
+        
+    },
+
+    preload: function () { 
+    },
+
+    create: function () { 
+        game.add.bitmapText(25, 85, 'bits-1', 'You Died - The End', 32);
+    },
+    update:function () {
+
+    }
+};
+},{}],6:[function(require,module,exports){
+var DataBase =  require('../utils/storage');
+var scoreDB;    
+
+module.exports = {
+    init: function () {
+        
+    },
+
+    preload: function () { 
+    },
+
+    create: function () { 
+        game.add.bitmapText(25, 85, 'bits-1', 'You Won - The End', 32);
+        
+    },
+    update:function () {
+
+    }
+};
+
+},{"../utils/storage":11}],7:[function(require,module,exports){
+module.exports = {
+    init: function () {
+        
+    },
+
+    preload: function () { 
+    },
+
+    create: function () { 
+        var intro = game.add.bitmapText(game.world.centerX, game.world.centerY, 'bits-1', 'Blockrunner', 32);
+        var width = intro.width;
+        var height = intro.height;
+
+        intro.x = game.world.centerX - width/2;
+        intro.y = game.world.centerY - height/2;
+        
+
+        
+  		game.time.events.loop(Phaser.Timer.SECOND/3, function () {
+  			intro.tint =  Math.random() * 0xffffff;
+  		} , this);
+        game.time.events.add(Phaser.Timer.SECOND * 4, function () {
+			game.state.start('load');
+        }, this);
+
+    },
+    update:function () {
+
+    }
+};
+},{}],8:[function(require,module,exports){
 module.exports = {
     loadingLabel: function () {
         //Here we add a label to let the user know we are loading everything
@@ -887,8 +963,15 @@ module.exports = {
         game.load.image('powerup', 'assets/power.png');
         game.load.image('player-life', 'assets/player-middle.png');
         game.load.spritesheet('button', 'assets/buttons.png', 248, 64);
+        game.load.bitmapFont('bits-0', 'assets/fonts/bits0.png', 'assets/fonts/bits0.fnt');
+        
+        game.load.bitmapFont('bits-2', 'assets/fonts/bits2.png', 'assets/fonts/bits2.fnt');
         game.load.text('level-1', 'levels/level-1.txt');
         game.load.text('level-2', 'levels/level-2.txt');
+        game.load.text('level-3', 'levels/level-3.txt');
+        game.load.text('level-4', 'levels/level-4.txt');
+        game.load.text('level-5', 'levels/level-5.txt');
+        game.load.text('level-6', 'levels/level-6.txt');
     },
 
     create: function () {
@@ -896,28 +979,65 @@ module.exports = {
     }
 };
 
-},{}],6:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 var button;
 var half_button_width = 124;
 var half_button_height = 32;
-var actionOnClick = function () {
-	console.log('click');
-	game.state.start('level-1');
+var actionOnClick = function (levelButton) {
+    console.log(levelButton.levelSelect);
+    var levelNumber = levelButton.text.split(' ')[1];
+    var level = levels.filter(function (item) {
+        console.log(item);
+        return  levelNumber == item.level;
+    });
+    if(level.length === 0) return;
+	console.log(level);
+    console.log(levelButton);
+    console.log(levelButton.text);
+    console.log('level-' + level[0].level);
+    game.globals.LEVELNUMBER =level[0].level;
+    game.globals.LEVEL = 'level-' + level[0].level;
+	game.state.start('level-' + level[0].level);
 };
 var pad_clicked = false; 
+var levels;
+var over = function (levelText) {
 
+    levelText.tint = 0xF9FE99;
+    levelText.x = levelText.x+2;
+    levelText.y = levelText.y+2;
+};
+var out = function (levelText) {
+    levelText.tint = 0xFFFFFF;
+    levelText.x = levelText.x-2;
+    levelText.y = levelText.y-2;
+}
+
+var addLevelButton = function (text, x, y) {
+    var lb = game.add.bitmapText(x, y, 'bits-0','Level ' + text,32);
+    lb.inputEnabled = true;
+    lb.levelSelect = text
+    lb.events.onInputOver.add(over, this);
+    lb.events.onInputOut.add(out, this); 
+    lb.events.onInputDown.add(actionOnClick, this); 
+}
 module.exports = {
     create: function(){
     //This is just like any other Phaser create function
     //game.state.start('play');
 
-    	button = game.add.button(half_button_width, (game.world.centerY - half_button_height), 'button', actionOnClick, this, 1, 0, 2);
-
-
-		//button.onInputOver.add(over, this);
-		//button.onInputOut.add(out, this);
-		//button.onInputUp.add(up, this);
-
+    	//button = game.add.button(half_button_width, (game.world.centerY - half_button_height), 'button', actionOnClick, this, 1, 0, 2);
+        var DataBase = require('../utils/storage');
+        var db = new DataBase('levels');
+ 
+        levels = db.getAllLevelsCleared();
+        console.log(levels)
+        addLevelButton('1',32,16);
+        addLevelButton('2',32*6,16);
+        addLevelButton('3',32*12,16);
+        addLevelButton('4',32,16*5);
+        addLevelButton('5',32*6,16*5);
+        addLevelButton('6',32*12,16*5);
 		game.input.gamepad.start();
 
 		pad = game.input.gamepad.pad1;
@@ -936,7 +1056,7 @@ module.exports = {
     },
 };
 
-},{}],7:[function(require,module,exports){
+},{"../utils/storage":11}],10:[function(require,module,exports){
 var player;
 var blocks;
 var enemies;
@@ -959,7 +1079,9 @@ var scoreboard;
 var powerupCount = 0;
 var peopleCount = 0;
 var score = 0;
-
+var DataBase =  require('../utils/storage');
+var db; 
+var scoreDB;      
 function randomInt(max) {
     return Math.floor(Math.random() * max);
     return rdg.integerInRange(0, max);
@@ -1023,15 +1145,9 @@ var moveToPoint = function (actor, target) {
     var actX = (actor.x/game.globals.TILE_SIZE).toFixed();
     var actY = (actor.y/game.globals.TILE_SIZE).toFixed();
 
-    var move = game.add.tween(actor);
-    var targetDirection = {
-        x: (actX + target.x) * game.globals.TILE_SIZE,
-        y: (actY + target.y) * game.globals.TILE_SIZE 
-    };
+    findPathTo(actor, actX, actY, target.x, target.y);
+ 
 
-    move.to(targetDirection, 300, Phaser.Easing.Linear.None);
-    actor.activeTween = move;
-    actor.activeTween.start(); 
 
 }
 var findPathTo =  function pathTastic (actor, startx, starty, tilex, tiley) { 
@@ -1058,9 +1174,9 @@ var findPathTo =  function pathTastic (actor, startx, starty, tilex, tiley) {
         actor.activeTween.start(); 
  
     });
-    //console.log([startx, starty], [tilex, tiley])
-    pathfinder.preparePathCalculation([Number(startx) , Number(starty)], [Number(tilex) ,Number(tiley)]);
+    
     try {
+        pathfinder.preparePathCalculation([Number(startx) , Number(starty)], [Number(tilex) ,Number(tiley)]);
         pathfinder.calculatePath();
     } catch (e) {
         console.log('error');
@@ -1077,10 +1193,7 @@ var isClose = function (point1, point2) {
     var point2Y = (point2.y/game.globals.TILE_SIZE).toFixed();
 
     var diffX = Math.abs(point1X - point2X);
-    var diffY = Math.abs(point1Y - point2Y);
-    //console.log(diffY);
-    //console.log(diffX);
-    //console.log((diffY < 6) && (diffX < 6))
+    var diffY = Math.abs(point1Y - point2Y); 
     return (diffY < 6) && (diffX < 6);
 };
 
@@ -1089,29 +1202,46 @@ var canGo = function (actor, dir) {
             actor.x+dir.x <= game.globals.WIDTH - 1 &&
             actor.y+dir.y >= 0 &&
             actor.y+dir.y <= game.globals.HEIGHT - 1 &&
-            map[actor.y+dir.y][actor.x +dir.x] == '.';
+            map[actor.y+dir.y][actor.x +dir.x] != '#';
 }
 
 var isClearSpot = function (spot) {
-    if( map === 'undefined') return false;
-    //console.log(spot);
-    //console.log(map);
-    return map[spot.x][spot.y] === '.';
+    if(spot.x >= game.globals.WIDTH || spot.y >= game.globals.HEIGHT) {
+        return false;
+    }
+    if( !map  ) { 
+        return false;
+    }
+    if (!map[spot.x] ) { 
+        return false
+    }
+    if(!map[spot.x][spot.y] ) { 
+        return false;
+    }
+    return map[spot.x][spot.y] != '#';
 };
-var getRandomSpot = function () {
+var getRandomSpot = function (x,y) { 
+    var xN = Number(x);
+    var yN = Number(y);
+    var RANGE = 5;
     return {
-        x:game.rnd.integerInRange(0, game.globals.HEIGHT-1),
-        y:game.rnd.integerInRange(0, game.globals.WIDTH-1)
+        x:game.rnd.integerInRange(Number(xN - RANGE), Number(xN + RANGE)),
+        y:game.rnd.integerInRange(Number(yN - RANGE), Number(yN + RANGE))
     };
 };
 
-var getOpenSpot = function () {
+var getOpenSpot = function (actor) {
 
-    var spot = getRandomSpot();
-    if(isClearSpot(spot)) {
+    var actX = (actor.x/game.globals.TILE_SIZE).toFixed();
+    var actY = (actor.y/game.globals.TILE_SIZE).toFixed();
+
+    var spot = getRandomSpot(actX, actY);
+    
+    console.log(spot);
+    if (isClearSpot(spot)) {
         return spot;
     }
-    //getOpenSpot();
+    //return getOpenSpot(actor);
 }
 
 
@@ -1131,36 +1261,16 @@ var hunt =  function huntingSeason (badguy) {
     if (isClose(badguy, player.sprite)){
         moveTo(badguy, player.sprite);
     } else {
-
-        //moveTo(badguy, {x: badguy.x+32, y:badguy.y+32} );//random number
-        var spot = getOpenSpot();
-        if(spot == null) return;
-        moveToPoint(badguy, spot);
-        return;
-        if (diffX > diffY) {
-            if (diffX < 0) {
-                // left
-                moveToPoint(badguy, directions[0]);
-            } else {
-                // right
-                moveToPoint(badguy, directions[1]);
-            }
-        } else {
-            if (diffY < 0) {
-                // up
-                moveToPoint(badguy, directions[2]);
-            } else {
-                // down
-                moveToPoint(badguy, directions[3]);
-            }
-        }
+        var spot = getOpenSpot(badguy);
+        if (spot == null) return;
+        moveToPoint(badguy, spot); 
     }
- 
- 
 }
 
 module.exports = {
     create: function(){
+        db = new DataBase('levels');
+        scoreDB = new DataBase('scores');
 
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -1251,6 +1361,10 @@ module.exports = {
         player.on('died', function () {
 
             console.log('the player is dead');
+
+            scoreDB.saveScore(score);
+            game.state.start('dead');
+
         });
 
 
@@ -1270,7 +1384,7 @@ module.exports = {
         console.log(scoreboard);
         badguyBag.forEach(function (bad, index) {
 
-            game.time.events.loop(Phaser.Timer.SECOND, function () {
+            game.time.events.loop(800, function () {
                 hunt(bad);
             }, this);
         }); 
@@ -1319,49 +1433,68 @@ module.exports = {
         }, null, this);
 
         scoreboard.text = score.toString();
-        if(powerupCount === 0 && peopleCount === 0) {
+        if (powerupCount === 0 && peopleCount === 0) {
             console.log(game.globals.LEVELNUMBER);
             game.globals.LEVELNUMBER++;
+            db.saveLevel(game.globals.LEVELNUMBER);
             game.globals.LEVEL = 'level-'+game.globals.LEVELNUMBER;
             game.state.start(game.globals.LEVEL);
         }
-        if ((throttle % 100) === 0 ) {
-            badguyBag.forEach(function (bad, index){
-                if((index % 2) === 0) {
-                    hunt(bad);
-                }
-            }); 
-        }
-        if ((throttle % 135) === 0 ) {
-            badguyBag.forEach(function (bad, index) {
-                if((index % 2) !== 0) {
-                    hunt(bad);
-                }
-            });
-        }
-        /*
-        if ((throttle % 140) === 0 ) {
-            hunt(badguyBag[2]); 
-        }
-        if ((throttle % 160) === 0 ) {
-            hunt(badguyBag[3]); 
-        }
-        if ((throttle % 180) === 0 ) {
-            hunt(badguyBag[4]); 
-        }
-        if ((throttle % 230) === 0 ) {
-            hunt(badguyBag[5]); 
-        }
-        if ((throttle % 250) === 0 ) {
-            hunt(badguyBag[6]); 
-        }
-        if ((throttle % 270) === 0 ) {
-            hunt(badguyBag[7]); 
-        }
-        */
+ 
+
         player.update();
         throttle++;
     },
 };
 
-},{"../entities/player":3}]},{},[1]);
+},{"../entities/player":3,"../utils/storage":11}],11:[function(require,module,exports){
+//var depot = require('depot');
+ 
+var self;
+
+var DataBase = function (collection) {
+	self=this;
+
+	this.db = depot(collection);
+	console.log(this.db);
+
+
+}
+
+DataBase.prototype.getAllLevelsCleared = function (callback) {
+	var self = this;
+	var defaultLevel = {level:1, unlocked:Date.now()};
+	var levels = this.db.all();
+
+	console.log(levels);
+
+	if (levels.length === 0) {
+		this.db.save(defaultLevel);
+		levels.push(defaultLevel);
+	}
+
+	return levels;
+ 
+};
+
+DataBase.prototype.saveLevel = function(level) {
+	
+	this.db.save({level:level, unlocked:Date.now()});
+};
+
+DataBase.prototype.getTopScores = function () {
+	var scores = this.db.all();
+
+	scores.sort(function (score1, score2) {
+		return score1.score >= score2.score ? 1: -1;
+	});
+
+	return scores.slice(0,9);
+}
+
+DataBase.prototype.saveScore = function (score) {
+	this.db.save({score:score, recorded:Date.now()});
+};
+
+module.exports = DataBase;
+},{}]},{},[1]);
