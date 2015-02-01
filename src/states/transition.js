df080dbe9ef5be78885241307c2e92cdcbfe5fd5
Kevin Isom <kevin.isom@gmail.com>
2015-02-01 15:09:23 +1300

    Transition between levels

---
+++ src/states/transition.js
@@ -0,0 +1,34 @@
+module.exports = {
+    init: function () {
+        
+    },
+
+    preload: function () { 
+    },
+
+    create: function () { 
+        var intro = game.add.bitmapText(game.world.centerX, game.world.centerY, 'bits-1', 'Level Cleared', 32);
+        var width = intro.width;
+        var height = intro.height;
+
+        intro.x = game.world.centerX - width/2;
+        intro.y = game.world.centerY - height/2;
+  		game.time.events.loop(Phaser.Timer.SECOND/3, function () {
+  			intro.tint =  Math.random() * 0xffffff;
+  		}, this);
+  		game.time.events.add(Phaser.Timer.SECOND* 2, function () {
+  			intro.text = "Next Level";
+  			var nwidth = intro.width;
+        	var nheight = intro.height;
+        	intro.x = game.world.centerX - nwidth/2;
+        	intro.y = game.world.centerY - nheight/2;
+  		}, this);
+        game.time.events.add(Phaser.Timer.SECOND * 4, function () {
+			game.state.start(game.globals.LEVEL);
+        }, this);
+
+    },
+    update:function () {
+
+    }
+};
